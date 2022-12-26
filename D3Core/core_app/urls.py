46e6a9616466e752061f083e85f9d4cdc544720e
Luka Djordjevic <lukadjordjevicsm01@gmail.com>
2022-12-26 23:17:54 +0100

    #1 Set up base project structure

---
+++ D3Core/core_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    # Kada korisnik pogodi ovaj endpoint (http://127.0.0.1:8000/ucitavanje/plugin/ucitati_prodavnice_kod),
+    # doci ce do ucitavanja plugina, ciji je identifikator "ucitati_prodavnice_kod".
+]
