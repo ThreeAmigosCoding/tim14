be77e1772cfe4e4f979201f5c85cbcbfda262ebe
lukaDjordjevic01 <96748944+lukaDjordjevic01@users.noreply.github.com>
2023-05-21 21:17:57 +0200

    #14 Implemented graph search

--- D3Core/core_app/static/canvas_utils.js
+++ D3Core/core_app/static/canvas_utils.js
@@ -3,3 +3,14 @@ var panZoom = d3.select('svg#main-canvas-svg')
         d3.select("svg#main-canvas-svg g").attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
     }));
 
+document.getElementById('apply-search').addEventListener('click', function() {
+    let layoutType = location.href.split('/').pop();
+    let queryString = document.getElementById('search-input').value.trim();
+    if (queryString === "") return
+    location.href = "/search/" + layoutType + "/" + queryString;
+});
+
+document.getElementById('reset-graph-btn').addEventListener('click', function() {
+    let layoutType = location.href.split('/').pop();
+    location.href = "/reset/" + layoutType;
+});
