217e6887f716e7f4d211c1f410a5f53336ebc9d6
cuturic01 <miloscuturic8@gmail.com>
2023-06-10 13:38:12 +0200

    #15 Implemented file upload
    The user can now upload his own xml and json files.

--- D3Core/core_app/urls.py
+++ D3Core/core_app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('ucitavanje/xml/plugin/<str:id>', views.load_plugin_xml, name="xml_parse_plugin"),
     path('search/<str:layout_type>/<str:query_string>', views.search_graph, name="search_graph"),
     path('filter/<str:layout_type>/<str:operation>', views.filter_graph, name="filter_graph"),
-    path('reset/<str:layout_type>', views.reset_graph, name="reset_graph")
+    path('reset/<str:layout_type>', views.reset_graph, name="reset_graph"),
+    path('upload', views.upload, name="upload"),
 ]
